123a4ee60dc3c53e0d4a2540832677f1908b2695
Hang <hang.yin2017@outlook.com>
2021-04-04 17:04:17 +1000

    EnvUtils isDev() isUat() isProd()

--- src/main/java/com/demo/dynamodb/utils/EnvUtils.java
+++ src/main/java/com/demo/dynamodb/utils/EnvUtils.java
@@ -1,24 +1,34 @@
 package com.demo.dynamodb.utils;
 
 import com.demo.dynamodb.constants.Env;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 public class EnvUtils {
 
-	private String env;
+	private Env env;
 
 	@Value("${spring.profiles.active}")
 	public void setEnv(String activeEnv) {
-		this.env = activeEnv.toUpperCase();
+		try {
+			this.env = Env.valueOf(activeEnv.toUpperCase());
+		} catch (Exception e) {
+			this.env = Env.DEV;
+		}
 	}
 
-	public Env getEnv() {
-		try {
-			return Env.valueOf(env);
-		} catch (Exception e) {
-			return Env.DEV;
-		}
+	public boolean isDev() {
+		return this.env.equals(Env.DEV);
+	}
+
+	public boolean isUat() {
+		return this.env.equals(Env.UAT);
+	}
+
+	public boolean isProd() {
+		return this.env.equals(Env.PROD);
 	}
 }
